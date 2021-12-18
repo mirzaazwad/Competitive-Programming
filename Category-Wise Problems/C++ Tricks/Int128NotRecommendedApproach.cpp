da6349c3c81bff2ca35d073ef5327e8d398f1f10
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2021-12-18 14:23:31 +0600

    Create Int128NotRecommendedApproach.cpp

---
+++ Category-Wise Problems/C++ Tricks/Int128NotRecommendedApproach.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;   // You have to include these header files and use these namespaces.
+ 
+int main()
+{
+    //int128_t n, m;// Signed
+    uint128_t n, m;// Unsigned
+    cin >> n >> m;
+    cout << n + m << endl;
+    return 0;
+}
