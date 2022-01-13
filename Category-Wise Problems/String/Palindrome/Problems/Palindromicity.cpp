16b186cbef9f2d12d0fbfff1d9a4f066f89bc489
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-01-13 22:54:04 +0600

    Create Palindromicity.cpp

---
+++ Category-Wise Problems/String/Palindrome/Problems/Palindromicity.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+void palindrome(int n){
+        if(n==1){
+           cout<<"z";
+           return;
+       }
+       if(n==0)return;
+       int i=(-1+(int)sqrt(double(1+8*n)))/2;
+       for(int j=0;j<i;j++){
+           cout<<"z";
+       }
+       int rem=n-(i*(i+1))/2;
+       if(rem==1){
+           cout<<"a";
+           return;
+       }
+       if(rem==2){
+           cout<<"bc";
+           return;
+       }
+       if(rem>0){
+           cout<<"bc";
+           palindrome(rem-2);
+       }
+       return;
+}
+
+int main()
+{
+    fastio;
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+       int n;
+       cin>>n;
+       palindrome(n);
+       cout<<endl;
+    }
+    return 0;
+}
