f71ba7b4cba1f818094ea4e2d7c630b680d221f9
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-05-22 01:04:39 +0600

    Add files via upload

---
+++ SolvedProblems/Codeforces/A_BowWow_and_the_Timetable.cpp
@@ -0,0 +1,183 @@
+/*
+*Author: Mirza Mohammad Azwad
+*Institude: Islamic University of Technology
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+
+template<class integer>
+inline integer to_int(const string& s, size_t* idx = 0, int base = 10) {
+    size_t n = s.size(), i = idx ? *idx : 0; bool sign = false; integer x = 0;
+    if (s[i] == '-')
+        ++i, sign = true;
+    function<int(char)> char_to_digit = [&](char c) {
+        static const int d[] = {'a'-10,'0'};
+        return tolower(c)-d[isdigit(c)]; };
+    while (i < n)
+        x *= base, x += char_to_digit(s[i++]);
+    if (idx)
+        *idx = i;
+    return sign ? -x : x; }
+ 
+template<class integer>
+inline string to_string(integer x, int base = 10) {
+    bool sign = false; string s;
+    if (x < 0)
+        x = -x, sign = true;
+    function<char(int)> digit_to_char = [](int d) {
+        static const char c[] = {'a'-10,'0'};
+        return c[d < 10]+d; };
+    do
+        s += digit_to_char(x%base), x /= base;
+    while (x > 0);
+    if (sign)
+        s += '-';
+    reverse(s.begin(),s.end());
+    return s; }
+ 
+template<class integer>
+inline istream& read(istream& is, integer& x) {
+    string s; is >> s, x = to_int<integer>(s); return is; }
+ 
+template<class integer>
+inline ostream& write(ostream& os, integer x) { return os << to_string(x); }
+ 
+using  lll =   signed __int128;
+using ulll = unsigned __int128;
+ 
+inline istream& operator>>(istream& is,  lll &x) { return  read(is,x); }
+inline istream& operator>>(istream& is, ulll &x) { return  read(is,x); }
+inline ostream& operator<<(ostream& os,  lll  x) { return write(os,x); }
+inline ostream& operator<<(ostream& os, ulll  x) { return write(os,x); }
+ 
+#define v(x) vector<x>
+#define d(x) deque<x>
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvll;
+typedef vector<vector<pll> > vvpll;
+typedef vector<pll> vpll;
+typedef vector<pi> vpi;
+typedef deque<ll> dll;
+typedef deque<int> di;
+typedef deque<deque<int> > ddi;
+typedef deque<deque<ll> > ddll;
+typedef deque<deque<pll> > ddpll;
+typedef deque<pll> dpll;
+typedef deque<pi> dpi;
+ll MOD = 998244353;
+ll M = 1000000007;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define forsnx(i,s,e,x) for(ll i = s; i < e; i+=x)
+#define rforsnx(i,s,e,x) for(ll i = s; i >= e; i-=x)
+#define endl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<endl
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define sp " "
+#define srt(v) sort(v.begin(), v.end());
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define Case cout << "Case " << it << ": ";
+#define down cout<<endl;
+#define raw_readline(x) getline(cin,(x))
+#define readline(x) string (x);getline(cin,(x))
+#define raw_read(x) cin>>(x)
+#define raw_input(x) cin>>(x);cin.ignore()
+#define input(datatype,x) datatype (x);cin>>(x);cin.ignore()
+#define read(datatype,x) datatype (x);cin>>(x)
+#define print(x) cout<<(x)<<endl
+#define output(x) cout<<(x)<<sp
+#define YES output("YES")
+#define Yes output("Yes")
+#define yes output("yes")
+#define NO output("NO")
+#define No output("No")
+#define no output("no")
+#define ittc input(int,tc) ; for(int it=1;it<=tc;it++)
+#define printarray(arr,n) forn(i,n)output(arr[i]); \
+down \
+ 
+
+
+void solve(){
+    readline(s);
+    int len=s.length();
+    int one_count=0;
+    forn(i,len){
+        if(s[i]=='1')one_count++;
+    }
+    //four value refers to values that are multiples of 4
+    if(one_count>1){
+        if((len%2))print(len/2+1);
+        else print(len/2);
+        //len/2 helps us determine the maximum even position starting from the right to left
+        //this will help us find maximum four value excluding 4^0 as we find all even positions except 0
+        //the one is added to account for 4^0
+        //odd
+        //len/2+1 as for odd we have to take all the previous four values into account and add 1 for 4^0
+        //even
+        //just len/2 would do because in reality excluding 1 we have to take the previous four value so (len-1)/2
+        //but we have to add 1 too so doing len/2 would take that into account
+        //add 1 for 4^0
+    }
+    else{
+        print((len)/2);
+        //even
+        //we are expencting floor division, since len is 
+        //divisible by 2 doing len-1 gives us the previous four value but we have to add one to account for
+        //4^0 so instead of adding one just don't do (len-1)/2
+        //odd
+        //since len is odd means that in a previous position we had a 4 value so we account for all
+        //the four values  and then add 1 for 4^0
+    }
+    
+
+    
+}
+int main()
+{
+    fastio;
+    //ittc{
+        ////Case;
+        //solve();
+        //down
+    //}
+        solve();
+ return 0;
+}
