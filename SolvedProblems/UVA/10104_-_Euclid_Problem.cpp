14cbae7c6a3a20f21d4d3f7db87ef48bedb4263c
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-07-22 17:50:57 +0600

    Add files via upload

---
+++ SolvedProblems/UVA/10104_-_Euclid_Problem.cpp
@@ -0,0 +1,40 @@
+/*
+*Author: Mirza Mohammad Azwad
+*Institude: Islamic University of Technology
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+using namespace std;
+ 
+#define ll long long
+#define endl "\n"
+ 
+ int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+
+int main()
+{
+    fastio;
+    int a,b;
+    while(cin>>a>>b){
+        int x,y,g;
+        g=gcd(a,b,x,y);
+        cout<<x<<" "<<y<<" "<<g<<endl;
+    }
+    return 0;
+}
