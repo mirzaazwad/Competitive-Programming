d1bbd9c78c236037a310f4424c04a7c078e4c9ad
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-07-26 15:58:46 +0600

    Add files via upload

---
+++ SolvedProblems/SPOJ/Prime_or_Not.cpp
@@ -0,0 +1,119 @@
+/*
+*Author: Mirza Mohammad Azwad
+*Institude: Islamic University of Technology
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+using namespace std;
+ 
+#define ll long long
+#define endl "\n"
+
+/* Calculate ( a*b ) %c */
+ll mulmod(ll a,ll b,ll c)
+{
+    ll x = 0 , y = a%c ;
+    while(b>0)
+    {
+        if(b%2) x = (x+y)%c ;
+        y=(y*2)%c;
+        b/=2;
+    }
+    return x%c ;
+}
+
+/* Calculate ( a^b ) %c */
+ll bigmod(ll a,ll b,ll c)
+{
+    ll x = 1 , y = a%c ;
+    while( b > 0 )
+    {
+        if(b%2) x = mulmod(x,y,c) ;
+        y=mulmod(y,y,c);
+        b/=2;
+    }
+    return x%c ;
+}
+
+ 
+// This function is called for all k trials. It returns
+// false if n is composite and returns true if n is
+// probably prime.
+// d is an odd number such that  d*2<sup>r</sup> = n-1
+// for some r >= 1
+bool miillerTest(ll d, ll n)
+{
+    // Pick a random number in [2..n-2]
+    // Corner cases make sure that n > 4
+    ll a = 2 + rand() % (n - 4);
+ 
+    // Compute a^d % n
+    ll x = bigmod(a, d, n);
+ 
+    if (x == 1  || x == n-1)
+       return true;
+ 
+    // Keep squaring x while one of the following doesn't
+    // happen
+    // (i)   d does not reach n-1
+    // (ii)  (x^2) % n is not 1
+    // (iii) (x^2) % n is not n-1
+    while (d != n-1)
+    {
+        x = mulmod(x,x,n);
+        d *= 2;
+ 
+        if (x == 1)      return false;
+        if (x == n-1)    return true;
+    }
+ 
+    // Return composite
+    return false;
+}
+ 
+// It returns false if n is composite and returns true if n
+// is probably prime.  k is an input parameter that determines
+// accuracy level. Higher value of k indicates more accuracy.
+bool isPrime(ll n, ll k)
+{
+    // Corner cases
+    if (n <= 1 || n == 4)  return false;
+    if (n <= 3) return true;
+ 
+    // Find r such that n = 2^d * r + 1 for some r >= 1
+    ll d = n - 1;
+    while (d % 2 == 0)
+        d /= 2;
+ 
+    // Iterate given number of 'k' times
+    for (ll i = 0; i < k; i++)
+         if (!miillerTest(d, n))
+              return false;
+ 
+    return true;
+}
+  
+
+void solve(){
+    ll n;
+    cin>>n;
+    ll k=100;
+    if(isPrime(n,k))cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+}
+int main()
+{
+    fastio;
+    srand(time(NULL));
+    int t;cin>>t;
+    for(int i=1;i<=t;i++){
+        //cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;
+}
