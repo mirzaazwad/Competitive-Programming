4f71c187889790cf1de09208de338bdbaea79016
MirzaMohammadAzwad <mirzaazwad8@gmail.com>
2022-09-26 19:03:16 +0600

    Solving Madoka and Strange Thoughts

--- SolvedProblems/Codeforces/A_Madoka_and_Strange_Thoughts.cpp
+++ SolvedProblems/Codeforces/A_Madoka_and_Strange_Thoughts.cpp
@@ -8,31 +8,41 @@
 #define endl "\n"
 
 using namespace std;
+#define N 10000007
+bool isPrime[N+1];
+vector<ll>primeList;
 
+void sieve(){
+    for(int i=0;i<=N;i++)isPrime[i]=true;
+    for(int i=4;i<=N;i+=2)isPrime[i]=false;
+    isPrime[2]=true;
+    isPrime[0]=false;
+    isPrime[1]=false;
+    for(int i=3;i<=N;i+=2){
+        if(isPrime[i]){
+            for(int j=i+i;j<=N;j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+    for(int i=0;i<=N;i++){
+        if(isPrime[i])primeList.push_back(i);
+    }
+}
 
 
 
 void solve(){
     ll n;
     cin>>n;
-    int count=0;
-    for(auto u:primeList){
-        if(n%u==0){
-            count++;
-        }
-    }
-    if(n-3>=0){
-        cout<<n*n-(count*(count-1)+(n*2) - 4)<<endl;
-    }
-    else{
-        cout<<n*n-(count*(count-1)+n-1)<<endl;
-    }
+    cout<<n+((n/2)+(n/3))*2<<endl;
+    
 }
 
 int main()
 {
     fastio;
-    sieve(1000007);
+    sieve();
     int t;
     cin >> t;
     
