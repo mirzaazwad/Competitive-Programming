cbda0bdef5a18af0d0409a7bc6e2e4616a6003d1
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2022-10-07 20:34:24 +0600

    Adding Merge Sort Implementation

---
+++ CategoryWisePersonalNotesTutorialsAndProblemsSolved/BacktrackingAndRecursion/Recursion/Implementations/merge_sort.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void Merge(int arr[],int start,int mid,int end){
+   
+    int left_size=mid-start+1;
+    int right_size=end-mid;
+    //create temporary arrays
+    int left[left_size],right[right_size];
+    //split to temporary arrays
+    //copy the data to temporary array
+    for(int i=0;i<left_size;i++){
+        left[i]=arr[start+i];
+    }
+    for(int i=0;i<right_size;i++){
+        right[i]=arr[mid+1+i];
+    }
+     int left_ix(0),right_ix(0),res_ix=start;
+    while(left_ix<left_size && right_ix<right_size){
+        if(left[left_ix]<=right[right_ix]){
+            arr[res_ix]=left[left_ix];
+            left_ix++;
+        }
+        else{
+            arr[res_ix]=right[right_ix];
+            right_ix++;
+        }
+        res_ix++;
+    }
+
+    while(left_ix<left_size){
+        arr[res_ix]=left[left_ix];
+        left_ix++;
+        res_ix++;
+    }
+    while(right_ix<right_size){
+        arr[res_ix]=right[right_ix];
+        right_ix++;
+        res_ix++;
+    }
+
+
+}
+
+void Merge_Sort(int arr[], int start, int end)
+{
+    if(start>=end){
+        return;
+    }
+    int mid=(start+end)/2;
+    Merge_Sort(arr,start,mid);
+    Merge_Sort(arr,mid+1,end);
+    Merge(arr,start,mid,end);
+}
+
+
+
+
+int main(void){
+    int arr[10]={-1,-7,5,4,3,0,8,9,2,-3};
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    Merge_Sort(arr,0,9);
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+}
